d5cd6a8cc5f79e7ce822dfe02da5cc7315dfb33e
Graham Mackenzie <gmackenzie@commontime.com>
2017-09-08 10:33:00 +0100

    Read preferences direct from xml on android, to get correct case

--- src/android/com/commontime/cordova/plugins/preferences/Preferences.java
+++ src/android/com/commontime/cordova/plugins/preferences/Preferences.java
@@ -1,11 +1,16 @@
 package com.commontime.cordova.plugins.preferences;
 
+import android.content.res.XmlResourceParser;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Preferences extends CordovaPlugin {
@@ -16,15 +21,50 @@ public class Preferences extends CordovaPlugin {
 
     @Override
 	protected void pluginInitialize() {
-        preferencesJson = new JSONObject();
-        Map<String, String> map = preferences.getAll();
-        for( String key : map.keySet() ) {
-            try {
-                preferencesJson.put(key, map.get(key));
-            } catch (JSONException e) {
-                e.printStackTrace();
+		preferencesJson = new JSONObject();
+
+		final int identifier = cordova.getActivity().getResources().getIdentifier("config",
+				"xml", cordova.getActivity().getPackageName());
+		final XmlResourceParser parser = cordova.getActivity().getResources().getXml(identifier);
+
+		try {
+			for (int eventType = -1; eventType != XmlResourceParser.END_DOCUMENT; eventType = parser.next()) {
+
+                if (eventType == XmlResourceParser.START_TAG) {
+                    String s = parser.getName();
+
+					String prefName = null;
+					String prefValue = null;
+
+                    if (s.equals("preference")) {
+                        for (int attr = 0; attr < parser.getAttributeCount(); attr++) {
+                            String name = parser.getAttributeName(attr);
+                            String val = parser.getAttributeValue(attr);
+                            System.out.println();
+
+							if( name.equals( "name") ) {
+								prefName = val;
+							} else if( name.equals( "value") ) {
+								prefValue = val;
+							}
+
+							if( prefName != null && prefValue != null ) {
+								try {
+									preferencesJson.put(prefName, prefValue);
+								} catch (JSONException e) {
+									e.printStackTrace();
+								}
+								break;
+							}
+                        }
+                    }
+                }
             }
-        }
+		} catch (XmlPullParserException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
